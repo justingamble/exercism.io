31efad52667236821bd4c0225c3218a426a091d6
Justin Gamble <justin.gamble@ualberta.net>
2019-05-13 00:42:32 -0600

    Exercism: finished space_age

---
+++ elixir/space-age/space_age.exs
@@ -0,0 +1,50 @@
+defmodule SpaceAge do
+  @type planet ::
+          :mercury
+          | :venus
+          | :earth
+          | :mars
+          | :jupiter
+          | :saturn
+          | :uranus
+          | :neptune
+  @earth_seconds_in_year 31557600
+  @conversion_map %{mercury: 0.2408467,
+                    venus:   0.61519726,
+                    mars:    1.8808158,
+                    jupiter: 11.862615,
+                    saturn:  29.447498,
+                    uranus:  84.016846,
+                    neptune: 164.79132}
+
+  @doc """
+  Return the number of years a person that has lived for 'seconds' seconds is
+  aged on 'planet'.
+  """
+  @spec age_on(planet, pos_integer) :: float
+  def age_on(planet, seconds) do
+    earth_years = get_earth_years(seconds)
+    case planet do
+      :earth -> earth_years
+      _ -> earth_years / @conversion_map[planet]
+    end
+  end
+
+  defp get_earth_years(seconds) do
+    seconds / @earth_seconds_in_year
+  end
+
+  @doc """
+  format_num() is useful for debugging, if you want to use IO.puts/1
+  to see the number of years without having exponents or lots of decimals
+  involved in output.
+
+  ## Example
+
+    iex> SpaceAge.format_num(1_000_000_000 / 31557600)
+    31.69
+  """
+  defp format_num(float) do
+    :erlang.float_to_binary(float, [decimals: 2])
+  end
+end
