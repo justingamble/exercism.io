4528e890c8a3e9a7cbb741f7e87bc8d6fd029ea5
Justin Gamble <justin.gamble@ualberta.net>
2019-05-13 00:54:11 -0600

    Renamed map to 'org_period_relative_to_earth_years'

--- elixir/space-age/space_age.exs
+++ elixir/space-age/space_age.exs
@@ -9,13 +9,15 @@ defmodule SpaceAge do
           | :uranus
           | :neptune
   @earth_seconds_in_year 31557600
-  @conversion_map %{mercury: 0.2408467,
-                    venus:   0.61519726,
-                    mars:    1.8808158,
-                    jupiter: 11.862615,
-                    saturn:  29.447498,
-                    uranus:  84.016846,
-                    neptune: 164.79132}
+  @orb_period_relative_to_earth_years %{
+    mercury: 0.2408467,
+    venus:   0.61519726,
+    mars:    1.8808158,
+    jupiter: 11.862615,
+    saturn:  29.447498,
+    uranus:  84.016846,
+    neptune: 164.79132
+  }
 
   @doc """
   Return the number of years a person that has lived for 'seconds' seconds is
@@ -26,7 +28,7 @@ defmodule SpaceAge do
     earth_years = get_earth_years(seconds)
     case planet do
       :earth -> earth_years
-      _ -> earth_years / @conversion_map[planet]
+      _ -> earth_years / @orb_period_relative_to_earth_years[planet]
     end
   end
 
