f54d74f76f3bbf798756e47fca9de4867766fc26
Justin Gamble <justin.gamble@ualberta.net>
2019-05-25 00:54:31 -0600

    rna-transcription: version1.  tests pass.

---
+++ elixir/rna-transcription/rna_transcription.exs
@@ -0,0 +1,23 @@
+defmodule RNATranscription do
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> RNATranscription.to_rna('ACTG')
+  'UGAC'
+  """
+  @spec to_rna([char]) :: [char]
+  def to_rna(dna) do
+    Enum.reduce(dna, [], fn x, acc -> result = acc ++ transcribe(x) end)
+  end
+
+  defp transcribe(?G), do: [?C]
+  defp transcribe(?C), do: [?G]
+  defp transcribe(?T), do: [?A]
+  defp transcribe(?A), do: [?U]
+  defp transcribe(unknown) do
+    raise("Unknown DNA characters: #{inspect unknown}\n")
+  end
+
+end
