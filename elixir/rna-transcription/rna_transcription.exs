1511623d00bf5ee4183d926ba1aae1c2b42d53c2
Justin Gamble <justin.gamble@ualberta.net>
2019-05-26 21:29:45 -0600

    rna_transcription: added older version of function for comparison

--- elixir/rna-transcription/rna_transcription.exs
+++ elixir/rna-transcription/rna_transcription.exs
@@ -16,4 +16,17 @@ defmodule RNATranscription do
   defp transcribe(?C), do: ?G
   defp transcribe(?T), do: ?A
   defp transcribe(?A), do: ?U
+
+
+  ## Alternate solution (earlier version of this same solution).
+  ## This alternate solution uses Enum.reduce/3 instead of Enum.map/2
+
+  # def to_rna(dna) do
+  #    Enum.reduce(dna, [], fn x, acc -> acc ++ transcribe(x) end)
+  # end
+  #
+  # defp transcribe(?G), do: [?C]
+  # defp transcribe(?C), do: [?G]
+  # defp transcribe(?T), do: [?A]
+  # defp transcribe(?A), do: [?U]
 end
