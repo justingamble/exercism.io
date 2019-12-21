cfcb49c84a4723c0cf5b1192cab208ec683ccf0d
Justin Gamble <justin.gamble@ualberta.net>
2019-12-21 04:06:57 -0700

    Exercism.io: found a clearner solution for word_count puzzle

--- elixir/word-count/lib/words.ex
+++ elixir/word-count/lib/words.ex
@@ -8,12 +8,14 @@ defmodule Words do
   def count(sentence) do
     sentence
     |> String.downcase
-    |> split_into_list
+    |> String.replace("_", " ")
+    |> filter_out_invalid_chars
+    |> String.split
     |> create_map_counts
   end
 
-  defp split_into_list(sentence) do
-    String.split(sentence, ~r/[[:blank:]_!@#$%^&*()+=:,]/u, trim: true)
+  defp filter_out_invalid_chars(sentence) do
+    String.replace(sentence, ~r/[^\w\s\-]/u, "")
   end
 
   defp create_map_counts(list) do
@@ -23,4 +25,28 @@ defmodule Words do
   defp word_count(word, acc_map) do
     Map.update(acc_map, word, 1, &(&1 + 1))
   end
+
+  ## Alternatively, the below also works, but I prefer the above because
+  ## a) the String.split parameters are simpler, and
+  ## b) it does not hardcode all the characters we want to exclude.
+
+  # @spec count(String.t()) :: map
+  # def count(sentence) do
+  #   sentence
+  #   |> String.downcase
+  #   |> split_into_list
+  #   |> create_map_counts
+  # end
+  #
+  # defp split_into_list(sentence) do
+  #   String.split(sentence, ~r/[[:blank:]_!@#$%^&*()+=:,]/u, trim: true)
+  # end
+  #
+  # defp create_map_counts(list) do
+  #   Enum.reduce(list, %{}, &word_count/2 )
+  # end
+  #
+  # defp word_count(word, acc_map) do
+  #   Map.update(acc_map, word, 1, &(&1 + 1))
+  # end
 end
